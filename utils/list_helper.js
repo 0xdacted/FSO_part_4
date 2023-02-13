110eaea88e654bbd1f00f8a636d80472ff548694
Red <crabmarket56@gmail.com>
2023-02-13 16:57:07 +0900

    Create testing for blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,23 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  let maxLikes = 0
+  let favorite = {}
+
+  for (const blog of blogs) {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes
+      favorite = blog
+    }
+  }
+
+  return favorite
+}
 
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
