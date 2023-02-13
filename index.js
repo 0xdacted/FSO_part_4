fd633424c89f825e8fadd6ede81dfb438c9fd869
Red <crabmarket56@gmail.com>
2023-02-13 15:33:40 +0900

    Add routes to blogs controller

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const logger = require('./utils/logger')
 
 const mongoose = require('mongoose')
 
-
-
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -20,23 +18,7 @@ mongoose.connect(MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
