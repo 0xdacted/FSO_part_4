d3f9b0985eddb0cb853cae9e011686d9c078c290
Red <crabmarket56@gmail.com>
2023-02-17 11:41:16 +0900

    Add 'AddUser' test

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -1,10 +1,25 @@
-const helper = require('../utils/list_helper')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const User = require('../models/user')
 const mongoose = require('mongoose')
 
+describe('AddUser', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+  })
+
+  test('creates a new user with valid input', async () => {
+    const user = {
+      username: 'testuser',
+      password: 'testpassword123',
+      name: 'Test User',
+    }
+    const response = await api.post('/api/users').send(user).expect(200)
+    expect(response.body.username).toBe(user.username)
+  })
+
+})
 
 
 afterAll(async () => {
